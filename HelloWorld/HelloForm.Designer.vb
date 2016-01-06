0bdc206b1cf3396a82c304e423b27a54a637ebf8
Victoria Brown <victoriabrown0710@gmail.com>
2016-01-06 10:50:17 -0600

    Initial Commit

---
+++ HelloWorld/HelloForm.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class HelloForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MessageLabel = New System.Windows.Forms.Label()
+        Me.PushButton = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SpanishButton = New System.Windows.Forms.Button()
+        Me.PrintButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'MessageLabel
+        '
+        Me.MessageLabel.Font = New System.Drawing.Font("Segoe Print", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MessageLabel.Location = New System.Drawing.Point(97, 67)
+        Me.MessageLabel.Name = "MessageLabel"
+        Me.MessageLabel.Size = New System.Drawing.Size(192, 31)
+        Me.MessageLabel.TabIndex = 0
+        Me.MessageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PushButton
+        '
+        Me.PushButton.Location = New System.Drawing.Point(102, 122)
+        Me.PushButton.Name = "PushButton"
+        Me.PushButton.Size = New System.Drawing.Size(76, 37)
+        Me.PushButton.TabIndex = 1
+        Me.PushButton.Text = "English"
+        Me.PushButton.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(213, 165)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(76, 37)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(265, 282)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(105, 35)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "by Victoria Brown"
+        '
+        'SpanishButton
+        '
+        Me.SpanishButton.Location = New System.Drawing.Point(103, 165)
+        Me.SpanishButton.Name = "SpanishButton"
+        Me.SpanishButton.Size = New System.Drawing.Size(75, 37)
+        Me.SpanishButton.TabIndex = 4
+        Me.SpanishButton.Text = "Spanish"
+        Me.SpanishButton.UseVisualStyleBackColor = True
+        '
+        'PrintButton
+        '
+        Me.PrintButton.Location = New System.Drawing.Point(213, 122)
+        Me.PrintButton.Name = "PrintButton"
+        Me.PrintButton.Size = New System.Drawing.Size(75, 37)
+        Me.PrintButton.TabIndex = 5
+        Me.PrintButton.Text = "Print"
+        Me.PrintButton.UseVisualStyleBackColor = True
+        '
+        'HelloForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(382, 314)
+        Me.Controls.Add(Me.PrintButton)
+        Me.Controls.Add(Me.SpanishButton)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.PushButton)
+        Me.Controls.Add(Me.MessageLabel)
+        Me.Name = "HelloForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Hello World by Victoria Brown"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents MessageLabel As Label
+    Friend WithEvents PushButton As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents SpanishButton As Button
+    Friend WithEvents PrintButton As Button
+End Class
---
+++ HelloWorld/HelloForm.vb
@@ -0,0 +1,29 @@
+﻿' Project:      Hello World
+' Programmer:   Victoria Brown
+' Date:         01/05/2015
+' Description:  This project will display a "Hello World" message in English and 
+'               Spanish.
+
+Public Class HelloForm
+    Private Sub PushButton_Click(sender As Object, e As EventArgs) Handles PushButton.Click
+        ' Display the Hello World message.
+
+        MessageLabel.Text = "Hello World"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' Exit the project.
+
+        Me.Close()
+    End Sub
+
+    Private Sub HelloForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub SpanishButton_Click(sender As Object, e As EventArgs) Handles SpanishButton.Click
+        ' Display the Hello World message in Spanish.
+
+        MessageLabel.Text = "Hola Mundo"
+    End Sub
+End Class

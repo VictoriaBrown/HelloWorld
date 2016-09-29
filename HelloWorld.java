6a36d20a9234df128ece3c216b8db6b57214de0c
Victoria Brown <victoriabrown0710@gmail.com>
2016-09-29 13:57:57 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/* Hello World
+Programmer:       Victoria Brown
+Date:             September 2016
+Simple Hello World to have in Github for Java */
+
+public class HelloWorld {
+  
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
